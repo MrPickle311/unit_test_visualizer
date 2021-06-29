93ebb0bf11dda2bfa62fab0161bc0d0a13150e97
MrPickle311 <macmacroll@gmail.com>
2021-06-29 12:51:24 +0200

    i have finished writing PortFlowSettings class and its prototype pattern

--- include/backend/Converter.hpp
+++ include/backend/Converter.hpp
@@ -5,6 +5,7 @@
 #include <QList>
 #include <functional>
 #include <QSharedPointer>
+#include <QMap>
 
 class PortScanner : public QObject
 {
@@ -31,9 +32,54 @@ class DataHandler : public QObject
 private:
     QByteArray received_bytes_;
 public:
-    void receiveBytes(QByteArray array);
+    void       receiveBytes(QByteArray array);
+    QByteArray getReceivedBytes();
 };
 
+class PortFlowSettings
+{
+    friend class PortOperator;
+private:
+    QSerialPort::BaudRate    baud_rate_;
+    QSerialPort::DataBits    data_bits_;
+    QSerialPort::FlowControl flow_control_;
+    QSerialPort::Parity      parity_;
+    QSerialPort::StopBits    stop_bits_;
+public:
+    PortFlowSettings(QSerialPort::BaudRate    baud_rate    ,
+                     QSerialPort::DataBits    data_bits    ,
+                     QSerialPort::FlowControl flow_control ,
+                     QSerialPort::Parity      parity       ,
+                     QSerialPort::StopBits    stop_bits  );
+    PortFlowSettings cloneSettings() const;
+};
+
+enum class StandardSetting : size_t
+{
+    StandardSetting9600,
+    StandardSetting115200
+};
+
+class StandardSettings
+{
+private:
+    QMap<StandardSetting,PortFlowSettings> standard_settings_;
+public:
+    StandardSettings()
+    {
+        standard_settings_[StandardSetting::StandardSetting9600] =
+        {QSerialPort::Baud9600 , QSerialPort::Data8 ,
+         QSerialPort::NoFlowControl , QSerialPort::NoParity ,
+         QSerialPort::OneStop};
+
+        standard_settings_[StandardSetting::StandardSetting115200] =
+        {QSerialPort::Baud115200 , QSerialPort::Data8 ,
+         QSerialPort::NoFlowControl , QSerialPort::NoParity ,
+         QSerialPort::OneStop};
+    }
+
+    PortFlowSettings getStandardSettings(StandardSetting setting) const;
+};
 
 class PortOperator : public QObject
 {
@@ -51,6 +97,8 @@ public:
     void sendDataFromPortToHandler();
     void enableReceivingData();
     void disableReceivingData();
+public slots:
+    void applySettings(PortFlowSettings settings);
 };
 
 class PortOperatorCreator

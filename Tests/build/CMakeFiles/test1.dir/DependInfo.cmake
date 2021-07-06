59b1e995a6f03e5e044bd19b895550b4f8a66fe7
MrPickle31 <macmacroll@gmail.com>
2021-07-06 22:43:54 +0200

    done some work

---
+++ Tests/build/CMakeFiles/test1.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/damiano/Projects/unit_test_visualizer/extern/googletest/googlemock/src/gmock-all.cc" "/home/damiano/Projects/unit_test_visualizer/Tests/build/CMakeFiles/test1.dir/home/damiano/Projects/unit_test_visualizer/extern/googletest/googlemock/src/gmock-all.cc.o"
+  "/home/damiano/Projects/unit_test_visualizer/extern/googletest/googletest/src/gtest-all.cc" "/home/damiano/Projects/unit_test_visualizer/Tests/build/CMakeFiles/test1.dir/home/damiano/Projects/unit_test_visualizer/extern/googletest/googletest/src/gtest-all.cc.o"
+  "/home/damiano/Projects/unit_test_visualizer/Tests/main.cpp" "/home/damiano/Projects/unit_test_visualizer/Tests/build/CMakeFiles/test1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GTEST_LANGUAGE_CXX11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/damiano/Projects/unit_test_visualizer/extern/googletest/googletest"
+  "/home/damiano/Projects/unit_test_visualizer/extern/googletest/googletest/include"
+  "/home/damiano/Projects/unit_test_visualizer/extern/googletest/googlemock"
+  "/home/damiano/Projects/unit_test_visualizer/extern/googletest/googlemock/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Tests/build/CMakeFiles/test1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test1.dir/home/damiano/Projects/unit_test_visualizer/extern/googletest/googlemock/src/gmock-all.cc.o"
+  "CMakeFiles/test1.dir/home/damiano/Projects/unit_test_visualizer/extern/googletest/googletest/src/gtest-all.cc.o"
+  "CMakeFiles/test1.dir/main.cpp.o"
+  "test1"
+  "test1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

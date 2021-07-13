39806e8415140e135af6ea79165148f11a25b432
MrPickle311 <macmacroll@gmail.com>
2021-07-13 22:41:46 +0200

    implemented ParsedDataPackage data structure

---
+++ modules/backend/parser/ParsedDataPackage.cpp
@@ -0,0 +1,39 @@
+#include "ParsedDataPackage.hpp"
+
+const QByteArray& ByteStorage::getbytes() const
+{
+    return bytes_;
+}
+
+void ByteStorage::setbytes(const QByteArray& newBytes)
+{
+    bytes_ = newBytes;
+}
+
+///
+
+bool ParsedDataPackage::isLeaf()
+{
+    return is_leaf_;
+}
+
+void ParsedDataPackage::setIsLeaf(bool newIs_leaf)
+{
+    is_leaf_ = newIs_leaf;
+}
+
+QSharedPointer<ParsedDataPackage> ParsedDataPackage::getParent()
+{
+    return parent_;
+}
+
+void ParsedDataPackage::setParent(QSharedPointer<ParsedDataPackage> newParent)
+{
+    parent_.reset(newParent.data());
+}
+
+void ParsedDataPackage::addChildren(QSharedPointer<ParsedDataPackage> child)
+{
+    children_.push_back(child);
+}
+
---
+++ modules/backend/parser/ParsedDataPackage.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <QList>
+#include <QSharedPointer>
+#include <memory>
+
+//class Composite
+//{
+//public:
+//    virtual bool       isLeaf()                        = 0 ;//if its a leaf -> contains bytes
+//    virtual void       setIsLeaf(bool newIs_leaf)      = 0 ;
+//    virtual Composite* getParent()                     = 0 ;
+//    virtual void       setParent(Composite* newParent) = 0 ;
+//    virtual void       addChildren(Composite* child)   = 0 ;
+//};
+
+
+//byte specialized
+class ByteStorage
+{
+protected:
+    QByteArray bytes_;//if its composite , bytes_ is empty
+public:
+    const QByteArray& getbytes() const;
+    void setbytes(const QByteArray& newBytes);
+};
+
+class ParsedDataPackage:
+        public ByteStorage
+{
+private:
+    QSharedPointer<ParsedDataPackage>        parent_;
+    bool                                     is_leaf_;//if its true -> end of processing
+    QList<QSharedPointer<ParsedDataPackage>> children_;
+public:
+    bool isLeaf();//if its a leaf -> contains bytes
+    void setIsLeaf(bool newIs_leaf);
+
+    QSharedPointer<ParsedDataPackage> getParent();
+    void setParent(QSharedPointer<ParsedDataPackage> newParent);
+
+    void addChildren(QSharedPointer<ParsedDataPackage> child);
+};
+

d7d58e8f1a5871a74f7f1afc1f0653c72568e02d
MrPickle311 <macmacroll@gmail.com>
2021-07-25 01:05:35 +0200

    some deletions

--- modules/backend/port/PortSettings.cpp
+++ modules/backend/port/PortSettings.cpp
@@ -1,71 +1,7 @@
 #include "../PortSettings.hpp"
-#include <QMutexLocker>
-#include "../../global/GlobalFunctions.hpp"
 
 namespace backend
 {
-
-PortScanner::PortScanner():
-    avalaible_ports_{QSerialPortInfo::availablePorts()}
-
-{}
-
-template<typename DataType>
-QList<DataType> PortScanner::getSerialInfoList(SerialPortInfoMethod<DataType> method_to_call) const
-{
-    QList<DataType> data_list;
-
-    for(auto&& avalaible_port : this->avalaible_ports_)
-        data_list.push_back(method_to_call(avalaible_port));
-
-    return data_list;
-}
-
-QSerialPortInfo PortScanner::getPortByNumber(uint port_nmbr) const
-{
-    throwIf(port_nmbr >= static_cast<uint>(avalaible_ports_.size()),
-            "port_nmbr >= avalaible ports number!");
-
-    return avalaible_ports_[port_nmbr];
-}
-
-QSerialPortInfo PortScanner::getPortByName(QString port_name) const
-{
-    for(auto&& port : avalaible_ports_)
-        if(port.portName().contains(port_name))
-            return port;
-    return QSerialPortInfo{};
-}
-
-QList<int> PortScanner::getProductIndetifiers() const
-{
-    return getSerialInfoList<int>(&QSerialPortInfo::productIdentifier);
-}
-
-QStringList PortScanner::getPortNames() const
-{
-   return getSerialInfoList<QString>(&QSerialPortInfo::portName);
-}
-
-QStringList PortScanner::getPortDescriptions() const
-{
-    return getSerialInfoList<QString>(&QSerialPortInfo::description);
-}
-
-QStringList PortScanner::getCompletePortData() const
-{
-    return global::joinStringListElements( getPortNames() , getPortDescriptions() );
-}
-
-void PortScanner::rescan()
-{
-    avalaible_ports_.clear();
-    avalaible_ports_ = QSerialPortInfo::availablePorts();
-}
-
-
-
-//port flow settings
 
 const QSerialPort::BaudRate& PortFlowSettings::baudRate() const
 {
@@ -134,39 +70,30 @@ PortFlowSettings PortFlowSettings::cloneSettings() const
     return *this;
 }
 
-void StandardSettings::initSettings()
+}
+
+namespace global
 {
-    standard_settings_[StandardSetting::StandardSetting9600] =
+
+template<>
+void backend::StandardSettings::initValues()
+{
+    variables_[backend::StandardSetting::StandardSetting9600] =
     {QSerialPort::Baud9600 , QSerialPort::Data8 ,
      QSerialPort::NoFlowControl , QSerialPort::NoParity ,
      QSerialPort::OneStop};
 
-    standard_settings_[StandardSetting::StandardSetting57600] =
+    variables_[backend::StandardSetting::StandardSetting57600] =
     {QSerialPort::Baud57600 , QSerialPort::Data8 ,
      QSerialPort::NoFlowControl , QSerialPort::NoParity ,
      QSerialPort::OneStop};
 
-    standard_settings_[StandardSetting::StandardSetting115200] =
+    variables_[backend::StandardSetting::StandardSetting115200] =
     {QSerialPort::Baud115200 , QSerialPort::Data8 ,
      QSerialPort::NoFlowControl , QSerialPort::NoParity ,
      QSerialPort::OneStop};
 }
 
-StandardSettings::StandardSettings()
-{}
-
-QMutex StandardSettings::mutex_{};
-QMap<StandardSetting,PortFlowSettings> StandardSettings::standard_settings_;
-bool StandardSettings::settings_initialized_{false};
-
-
-PortFlowSettings StandardSettings::getStandardSettings(StandardSetting setting)
-{
-    QMutexLocker lock{&mutex_};
-    if(!settings_initialized_)
-        initSettings();
-    return standard_settings_[setting];
-}
 
 }
 
